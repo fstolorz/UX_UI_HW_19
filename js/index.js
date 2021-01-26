996e2d4810b70bb4f9a3fe17c830fa7bf935d00f
fstolorz <fstolorz@gmail.com>
2021-01-25 22:43:04 -0500

    My portfolio V3.

---
+++ js/index.js
@@ -0,0 +1,29 @@
+console.log("Hi Fiona");
+
+$('#givingforgood').on("mouseenter", function(){
+    $(this).animate({ width: "+=20", height: "+=20" });
+})
+.on('mouseleave', function(){
+    $(this).animate({width: "-=20", height: "-=20"});
+})
+
+$('#usps').on("mouseenter", function(){
+    $(this).animate({ width: "+=20", height: "+=20" });
+})
+.on('mouseleave', function(){
+    $(this).animate({width: "-=20", height: "-=20"});
+})
+
+$('#bookbar').on("mouseenter", function(){
+    $(this).animate({ width: "+=20", height: "+=20" });
+})
+.on('mouseleave', function(){
+    $(this).animate({width: "-=20", height: "-=20"});
+})
+
+$('#adventuroo').on("mouseenter", function(){
+    $(this).animate({ width: "+=20", height: "+=20" });
+})
+.on('mouseleave', function(){
+    $(this).animate({width: "-=20", height: "-=20"});
+})
